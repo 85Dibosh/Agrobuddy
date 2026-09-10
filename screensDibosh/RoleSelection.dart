dd88323f892c75f3aa374345f8eccaa6cb48782a
85doi <diboshchowdhury85@gmail.com>
2026-09-10 15:11:36 +0600

    moved screens to screens folder

--- screensDibosh/RoleSelection.dart
+++ screensDibosh/RoleSelection.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 
 class RoleselectionScreen extends StatefulWidget {
   const RoleselectionScreen({super.key});
+  @override
   State<RoleselectionScreen> createState() => _RoleSelectionScreenState();
 }
 
 class _RoleSelectionScreenState extends State<RoleselectionScreen>{
+  int selectedRole = 0;
+
+  @override
+  Widget build(BuildContext context){
+    
+  }
 
 }
