126dccd1e2615c9a63816734d75fbc593db2bb1a
MdMinhazurRahmanNishat <326622976+MdMinhazurRahmanNishat053@users.noreply.github.com>
2026-09-11 23:39:46 +0600

    Farmer Dashboard intro

---
+++ lib/screensNishat/farmer_dashboard.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FarmerDashboard extends StatefulWidget {
+  const FarmerDashboard({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
